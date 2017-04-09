5174e67ee2ec470a592d99c48c4e6c2a0f017491
duncanrudd <dunc@3drstudio.co.uk>
2017-04-09 20:37:36 +0100

    updates

--- systems/aardCow.py
+++ systems/aardCow.py
@@ -370,3 +370,247 @@ def publishJoints(rig, systems):
             pmc.parentConstraint(joint, j, mo=0)
             pmc.scaleConstraint(joint, j, mo=0)
 
+
+############################################
+# ROOSTER
+
+'''
+import pymel.core as pmc
+import drTools.core.coreUtils as coreUtils
+import drTools.systems.spine as drSpine
+import drTools.systems.systemUtils as systemUtils
+import drTools.systems.twistySegment as drTwistySegment
+import drTools.systems.head as drHead
+import drTools.systems.root as drRoot
+import drTools.systems.arm as drArm
+import drTools.systems.hoof as drHoof
+import drTools.systems.hand as drHand
+
+reload(coreUtils)
+reload(systemUtils)
+reload(drSpine)
+reload(drTwistySegment)
+reload(drHead)
+reload(drRoot)
+reload(drArm)
+reload(drHoof)
+reload(drHand)
+
+rigSystem = drRoot.DrRig(ctrlSize=3.0)
+
+
+# spine
+spine = drSpine.DrSpine(name='neck', numJoints=8,
+                                   start=pmc.PyNode('hip_GD'),
+                                   end=pmc.PyNode('chest_GD'),
+                                   rtHip=pmc.PyNode('rt_hip_GD'),
+                                   lfHip=pmc.PyNode('lf_hip_GD'),
+                                   rtShldr=pmc.PyNode('rt_shldr_GD'),
+                                   lfShldr=pmc.PyNode('lf_shldr_GD'),)
+
+p = systemUtils.plug(spine.bodyZero_grp, rigSystem.rootSystem.sockets['03'], name='spine')
+p.setParent(spine.plugs_grp)
+
+# right leg
+rtLegSystem = drArm.DrArm(name='rt_leg',
+                               joints=[pmc.PyNode('rt_hip_GD'),
+                                       pmc.PyNode('rt_knee_GD'),
+                                       pmc.PyNode('rt_ankle_GD'),
+                                       pmc.PyNode('rt_ankle_end_GD')],
+                               numTwistSegs=7,
+                               upAxis='z',
+                               flipTwist=0,
+                               leg=0)
+# connect to body
+p = systemUtils.plug(rtLegSystem.fkCtrls_grp, spine.sockets['rt_hip'], plugType='parent',
+                     name='rt_leg_hip')
+p.setParent(rtLegSystem.plugs_grp)
+
+# spaceswitch for ikCtrl
+p = systemUtils.multiPlug(rtLegSystem.ikCtrls_grp,
+                          targetList=[rigSystem.rootSystem.sockets['03'],
+                                      spine.sockets['hips']],
+                          targetNames=['root', 'hip'],
+                          settingsNode=rtLegSystem.ikCtrl,
+                          plugType='parent',
+                          name='rt_leg_ikCtrl')
+p.setParent(rtLegSystem.plugs_grp)
+
+# spaceswitch for polevector
+p = systemUtils.multiPlug(rtLegSystem.pvZero,
+                          targetList=[rigSystem.rootSystem.sockets['03'],
+                                      spine.sockets['hips'],
+                                      rtLegSystem.sockets['poleVectorAim']],
+                          targetNames=['root', 'hip', 'foot'],
+                          settingsNode=rtLegSystem.pvCtrl,
+                          plugType='parent',
+                          name='rt_leg_poleVectorCtrl')
+p.setParent(rtLegSystem.plugs_grp)
+
+# left leg
+lfLegSystem = drArm.DrArm(name='lf_leg',
+                               joints=[pmc.PyNode('lf_hip_GD'),
+                                       pmc.PyNode('lf_knee_GD'),
+                                       pmc.PyNode('lf_ankle_GD'),
+                                       pmc.PyNode('lf_ankle_end_GD')],
+                               numTwistSegs=7,
+                               upAxis='z',
+                               flipTwist=1,
+                               colour='blue',
+                               leg=0)
+# connect to body
+p = systemUtils.plug(lfLegSystem.fkCtrls_grp, spine.sockets['lf_hip'], plugType='parent',
+                     name='lf_leg_hip')
+p.setParent(lfLegSystem.plugs_grp)
+
+# spaceswitch for ikCtrl
+p = systemUtils.multiPlug(lfLegSystem.ikCtrls_grp,
+                          targetList=[rigSystem.rootSystem.sockets['03'],
+                                      spine.sockets['hips']],
+                          targetNames=['root', 'hip'],
+                          settingsNode=lfLegSystem.ikCtrl,
+                          plugType='parent',
+                          name='lf_leg_ikCtrl')
+p.setParent(lfLegSystem.plugs_grp)
+
+# spaceswitch for polevector
+p = systemUtils.multiPlug(lfLegSystem.pvZero,
+                          targetList=[rigSystem.rootSystem.sockets['03'],
+                                      spine.sockets['hips'],
+                                      lfLegSystem.sockets['poleVectorAim']],
+                          targetNames=['root', 'hip', 'foot'],
+                          settingsNode=lfLegSystem.pvCtrl,
+                          plugType='parent',
+                          name='lf_leg_poleVectorCtrl')
+p.setParent(lfLegSystem.plugs_grp)
+
+# left arm
+lfArmSystem = drArm.DrArm(name='lf_wing',
+                               joints=[pmc.PyNode('lf_shldr_GD'),
+                                       pmc.PyNode('lf_elbow_GD'),
+                                       pmc.PyNode('lf_wrist_GD'),
+                                       pmc.PyNode('lf_wrist_end_GD')],
+                               numTwistSegs=6,
+                               leg=0,
+                               flipTwist=1,
+                               colour='blue')
+# connect to body
+p = systemUtils.plug(lfArmSystem.fkCtrls_grp, spine.sockets['lf_shldr'], plugType='parent',
+                     name='shldr')
+p.setParent(lfArmSystem.plugs_grp)
+
+# spaceswitch for ikCtrl
+p = systemUtils.multiPlug(lfArmSystem.ikCtrls_grp,
+                          targetList=[rigSystem.rootSystem.sockets['03'], spine.sockets['body']],
+                          targetNames=['root', 'body'],
+                          settingsNode=lfArmSystem.ikCtrl,
+                          plugType='parent',
+                          name='lf_wing_ikCtrl')
+p.setParent(lfArmSystem.plugs_grp)
+
+# spaceswitch for polevector
+p = systemUtils.multiPlug(lfArmSystem.pvZero,
+                          targetList=[rigSystem.rootSystem.sockets['03'], spine.sockets['body'],
+                                      lfArmSystem.sockets['poleVectorAim']],
+                          targetNames=['root', 'body', 'arm'],
+                          settingsNode=lfArmSystem.pvCtrl,
+                          plugType='parent',
+                          name='lf_wing_poleVectorCtrl')
+p.setParent(lfArmSystem.plugs_grp)
+
+# right arm
+rtArmSystem = drArm.DrArm(name='rt_wing',
+                               joints=[pmc.PyNode('rt_shldr_GD'),
+                                       pmc.PyNode('rt_elbow_GD'),
+                                       pmc.PyNode('rt_wrist_GD'),
+                                       pmc.PyNode('rt_wrist_end_GD')],
+                               numTwistSegs=6,
+                               leg=0,
+                               flipTwist=0,
+                               colour='red')
+# connect to body
+p = systemUtils.plug(rtArmSystem.fkCtrls_grp, spine.sockets['rt_shldr'], plugType='parent',
+                     name='shldr')
+p.setParent(rtArmSystem.plugs_grp)
+
+# spaceswitch for ikCtrl
+p = systemUtils.multiPlug(rtArmSystem.ikCtrls_grp,
+                          targetList=[rigSystem.rootSystem.sockets['03'], spine.sockets['body']],
+                          targetNames=['root', 'body'],
+                          settingsNode=rtArmSystem.ikCtrl,
+                          plugType='parent',
+                          name='rt_wing_ikCtrl')
+p.setParent(rtArmSystem.plugs_grp)
+
+# spaceswitch for polevector
+p = systemUtils.multiPlug(rtArmSystem.pvZero,
+                          targetList=[rigSystem.rootSystem.sockets['03'], spine.sockets['body'],
+                                      rtArmSystem.sockets['poleVectorAim']],
+                          targetNames=['root', 'body', 'arm'],
+                          settingsNode=rtArmSystem.pvCtrl,
+                          plugType='parent',
+                          name='rt_wing_poleVectorCtrl')
+p.setParent(rtArmSystem.plugs_grp)
+
+# right foot
+rtFootSystem = drHoof.DrHoof(name='rt_foot',
+                                                ankle=pmc.PyNode('rt_ankle_GD'),
+                                                toe=pmc.PyNode('rt_toe_GD'),
+                                                heel=pmc.PyNode('rt_heel_GD'),
+                                                inner=pmc.PyNode('rt_inner_GD'),
+                                                outer=pmc.PyNode('rt_outer_GD'),
+                                                settingsNode=rtLegSystem.ikCtrl,
+                                                blendAttr=rtLegSystem.settingsCtrl.ik_fk_blend)
+
+# connect to body
+p = systemUtils.plug(rtFootSystem.ikConstGrp, rtLegSystem.sockets['ikCtrl'], name='rt_ikFoot')
+p.setParent(rtFootSystem.plugs_grp)
+p = systemUtils.plug(rtFootSystem.fkConstGrp, rtLegSystem.sockets['fkCtrl'], name='rt_fkFoot')
+p.setParent(rtFootSystem.plugs_grp)
+
+# attach ik leg to foot
+p = systemUtils.plug(rtLegSystem.ctrl_loc, rtFootSystem.sockets['ankle'], name='rt_leg_ikFoot')
+p.setParent(rtLegSystem.plugs_grp)
+
+# left foot
+lfFootSystem = drHoof.DrHoof(name='lf_foot',
+                                                ankle=pmc.PyNode('lf_ankle_GD'),
+                                                toe=pmc.PyNode('lf_toe_GD'),
+                                                heel=pmc.PyNode('lf_heel_GD'),
+                                                inner=pmc.PyNode('lf_inner_GD'),
+                                                outer=pmc.PyNode('lf_outer_GD'),
+                                                settingsNode=lfLegSystem.ikCtrl,
+                                                blendAttr=lfLegSystem.settingsCtrl.ik_fk_blend)
+
+# connect to body
+p = systemUtils.plug(lfFootSystem.ikConstGrp, lfLegSystem.sockets['ikCtrl'], name='lf_ikFoot')
+p.setParent(lfFootSystem.plugs_grp)
+p = systemUtils.plug(lfFootSystem.fkConstGrp, lfLegSystem.sockets['fkCtrl'], name='lf_fkFoot')
+p.setParent(lfFootSystem.plugs_grp)
+
+# attach ik leg to foot
+p = systemUtils.plug(lfLegSystem.ctrl_loc, lfFootSystem.sockets['ankle'], name='lf_leg_ikFoot')
+p.setParent(lfLegSystem.plugs_grp)
+
+# Parent to rig_grp and connect globalscales of all systems
+for system in [spine,
+               rtArmSystem,
+               lfArmSystem,
+               rtLegSystem,
+               lfLegSystem,
+               rtFootSystem,
+               lfFootSystem]:
+    system.main_grp.setParent(rigSystem.rig_grp)
+    rigSystem.rootSystem.main_grp.globalScale.connect(system.main_grp.globalScale)
+
+################################################################################################################
+# Publish joints
+publishJoints(rigSystem, [spine,
+                               rtArmSystem,
+                               lfArmSystem,
+                               rtLegSystem,
+                               lfLegSystem,
+                               rtFootSystem,
+                               lfFootSystem])
+'''
+
